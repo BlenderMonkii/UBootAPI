c836aceb76b3ca2a28f0169d1941fc690680e8ae
Ricardo Langer <ricardo.langer@stud.htw-dresden.de>
2025-01-20 21:52:19 +0100

    add boundingBox Model

--- UBootAPI/Model/BoundingBoxRequest.cs
+++ UBootAPI/Model/BoundingBoxRequest.cs
@@ -6,8 +6,6 @@
         public double MinLat { get; set; } = -5;
         public double MaxLon { get; set; } = 10;
         public double MaxLat { get; set; } = 5;
-        public int Width { get; set; } = 800;
-        public int Height { get; set; } = 600;
     }
 
 }
